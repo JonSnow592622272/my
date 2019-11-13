7b44360f7cd8cd9c711268680fe800658cfc5dc0
wulingming <wulingming@zhixueyun.com>
2019-11-13 10:57:57 +0800

    优化

--- my-service/src/main/java/com/shotgun/my/service/dao/defaultDb/UserMapper.java
+++ my-service/src/main/java/com/shotgun/my/service/dao/defaultDb/UserMapper.java
@@ -2,7 +2,7 @@ package com.shotgun.my.service.dao.defaultDb;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.shotgun.my.api.dto.User;
-import com.shotgun.my.service.dao.MyBaseMapper;
+import com.shotgun.mycommon.service.base.MyBaseMapper;
 
 import java.util.List;
 

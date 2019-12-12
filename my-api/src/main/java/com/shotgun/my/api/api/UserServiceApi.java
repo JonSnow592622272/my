8229dc567486d4a9c0f2bbc23d5c9b9840d81e34
wulingming <wulingming@zhixueyun.com>
2019-12-12 19:55:47 +0800

    优化

--- my-api/src/main/java/com/shotgun/my/api/api/UserServiceApi.java
+++ my-api/src/main/java/com/shotgun/my/api/api/UserServiceApi.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @FeignClient(name = CommonConstant.APPLICATION_NAME)
+//@RequestMapping("/xxx")
 public interface UserServiceApi {
 
     @GetMapping("/getOnelalala")

bb4fad72c1ad0c1ac24f569a0d3751c11d4969e3
wulingming <wulingming@zhixueyun.com>
2019-12-13 14:32:28 +0800

    优化

--- my-service/src/main/java/com/shotgun/my/service/service/UserService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/UserService.java
@@ -1,13 +1,10 @@
 package com.shotgun.my.service.service;
 
 import com.shotgun.my.api.api.UserServiceApi;
-import com.shotgun.my.api.consts.CommonConstant;
-import org.springframework.cloud.openfeign.FeignClient;
 
 /**
  * @desc 提供给controller层的接口放在Api接口，仅给service层提供的接口才放到这
  **/
-@FeignClient(name = CommonConstant.APPLICATION_NAME)
 public interface UserService extends UserServiceApi {
 
 

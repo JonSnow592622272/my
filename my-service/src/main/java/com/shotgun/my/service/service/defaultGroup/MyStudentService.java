eafe476076494f38822185105a87338aa03fffd2
wulingming <wulingming@zhixueyun.com>
2019-12-13 15:28:02 +0800

    修正编码格式

--- my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/MyStudentService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/MyStudentService.java
@@ -1,13 +1,11 @@
 package com.shotgun.my.service.service.defaultGroup;
 
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.shotgun.my.api.api.defaultGroup.MyStudentServiceApi;
-import com.shotgun.my.api.po.pojos.defaultGroup.MyStudent;
 
 /**
  * @desc 提供给controller层的接口放在Api接口，仅给service层提供的接口才放到这
  **/
-public interface MyStudentService extends MyStudentServiceApi, IService<MyStudent> {
+public interface MyStudentService extends MyStudentServiceApi {
 
 
 }

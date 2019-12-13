3d7520819397896b5543c610edb3264037714598
wulingming <wulingming@zhixueyun.com>
2019-12-13 15:01:32 +0800

    优化

--- my-service/src/main/java/com/shotgun/my/service/dao/defaultGroup/MyStudentMapper.java
+++ my-service/src/main/java/com/shotgun/my/service/dao/defaultGroup/MyStudentMapper.java
@@ -2,13 +2,11 @@ package com.shotgun.my.service.dao.defaultGroup;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shotgun.my.api.po.pojos.defaultGroup.MyStudent;
-import org.springframework.stereotype.Repository;
 
 /**
 * 学生表-Mapper 接口
 *
 */
-@Repository(value = "iMyStudentMapper")
 public interface MyStudentMapper extends BaseMapper<MyStudent> {
 
 }
--- my-service/src/main/java/com/shotgun/my/service/dao/defaultGroup/MyStudentScoresMapper.java
+++ my-service/src/main/java/com/shotgun/my/service/dao/defaultGroup/MyStudentScoresMapper.java
@@ -2,13 +2,11 @@ package com.shotgun.my.service.dao.defaultGroup;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shotgun.my.api.po.pojos.defaultGroup.MyStudentScores;
-import org.springframework.stereotype.Repository;
 
 /**
 * 分数表-Mapper 接口
 *
 */
-@Repository(value = "iMyStudentScoresMapper")
 public interface MyStudentScoresMapper extends BaseMapper<MyStudentScores> {
 
 }

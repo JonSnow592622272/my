039607d2816ebffdb7defc1c8a9e7419e6e0f29c
wulingming <wulingming@zhixueyun.com>
2019-12-13 16:06:53 +0800

    修正编码格式

--- my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/MyStudentScoresService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/MyStudentScoresService.java
@@ -1,11 +1,13 @@
 package com.shotgun.my.service.service.defaultGroup;
 
 import com.shotgun.my.api.api.defaultGroup.MyStudentScoresServiceApi;
+import com.shotgun.my.api.po.pojos.defaultGroup.MyStudentScores;
+import com.shotgun.mycommon.service.base.MyIService;
 
 /**
  * @desc 提供给controller层的接口放在Api接口，仅给service层提供的接口才放到这
  **/
-public interface MyStudentScoresService extends MyStudentScoresServiceApi {
+public interface MyStudentScoresService extends MyStudentScoresServiceApi, MyIService<MyStudentScores>  {
 
 
 }
--- my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/MyStudentService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/MyStudentService.java
@@ -1,11 +1,13 @@
 package com.shotgun.my.service.service.defaultGroup;
 
 import com.shotgun.my.api.api.defaultGroup.MyStudentServiceApi;
+import com.shotgun.my.api.po.pojos.defaultGroup.MyStudent;
+import com.shotgun.mycommon.service.base.MyIService;
 
 /**
  * @desc 提供给controller层的接口放在Api接口，仅给service层提供的接口才放到这
  **/
-public interface MyStudentService extends MyStudentServiceApi {
+public interface MyStudentService extends MyStudentServiceApi, MyIService<MyStudent>  {
 
 
 }

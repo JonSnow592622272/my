e1d0f8ceb52f262e141d2c71d1c3691c565f08f4
wulingming <wulingming@zhixueyun.com>
2019-12-16 19:49:32 +0800

    优化

--- my-api/src/main/java/com/shotgun/my/api/api/defaultGroup/MyStudentScoresServiceApi.java
+++ my-api/src/main/java/com/shotgun/my/api/api/defaultGroup/MyStudentScoresServiceApi.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.shotgun.my.api.consts.CommonConstant;
 import com.shotgun.my.api.po.pojos.defaultGroup.MyStudentScores;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = CommonConstant.APPLICATION_NAME)
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface MyStudentScoresServiceApi {
     String PATH = "/my_student_scores";
 
+    @GetMapping("/testGet10")
     IPage<MyStudentScores> testGet10();
 
 
--- my-api/src/main/java/com/shotgun/my/api/api/defaultGroup/MyTeacherServiceApi.java
+++ my-api/src/main/java/com/shotgun/my/api/api/defaultGroup/MyTeacherServiceApi.java
@@ -1,13 +1,19 @@
 package com.shotgun.my.api.api.defaultGroup;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.shotgun.my.api.consts.CommonConstant;
+import com.shotgun.my.api.po.pojos.defaultGroup.MyTeacher;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = CommonConstant.APPLICATION_NAME)
 @RequestMapping(MyStudentServiceApi.PATH)
 public interface MyTeacherServiceApi {
     String PATH = "/my_teacher";
+
+    @GetMapping("/testGet10")
+    IPage<MyTeacher> testGet10();
 
 
 }

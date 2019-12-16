46a10cf44f80a49b5663c416a3eed7e4c03f72e2
wulingming <wulingming@zhixueyun.com>
2019-12-16 11:07:14 +0800

    修正编码格式

--- my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyStudentScoresServiceImpl.java
+++ my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyStudentScoresServiceImpl.java
@@ -1,16 +1,16 @@
 package com.shotgun.my.service.serviceImpl.defaultGroup;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shotgun.my.api.api.defaultGroup.MyStudentScoresServiceApi;
 import com.shotgun.my.api.po.pojos.defaultGroup.MyStudentScores;
 import com.shotgun.my.service.dao.defaultGroup.MyStudentScoresMapper;
 import com.shotgun.my.service.service.defaultGroup.MyStudentScoresService;
+import com.shotgun.mycommon.service.base.MyServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(MyStudentScoresServiceApi.PATH)
-public class MyStudentScoresServiceImpl extends ServiceImpl<MyStudentScoresMapper, MyStudentScores> implements MyStudentScoresService {
+public class MyStudentScoresServiceImpl extends MyServiceImpl<MyStudentScoresMapper, MyStudentScores> implements MyStudentScoresService {
 
 
 }
--- my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyStudentServiceImpl.java
+++ my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyStudentServiceImpl.java
@@ -1,16 +1,16 @@
 package com.shotgun.my.service.serviceImpl.defaultGroup;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shotgun.my.api.api.defaultGroup.MyStudentServiceApi;
 import com.shotgun.my.api.po.pojos.defaultGroup.MyStudent;
 import com.shotgun.my.service.dao.defaultGroup.MyStudentMapper;
 import com.shotgun.my.service.service.defaultGroup.MyStudentService;
+import com.shotgun.mycommon.service.base.MyServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(MyStudentServiceApi.PATH)
-public class MyStudentServiceImpl extends ServiceImpl<MyStudentMapper, MyStudent> implements MyStudentService {
+public class MyStudentServiceImpl extends MyServiceImpl<MyStudentMapper, MyStudent> implements MyStudentService {
 
 
 }

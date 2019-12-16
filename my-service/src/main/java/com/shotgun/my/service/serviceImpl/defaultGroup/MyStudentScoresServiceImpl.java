d5a657b5c89a4d5e9a1e75a1fc3ad1a7d28e91c2
wulingming <wulingming@zhixueyun.com>
2019-12-16 20:16:29 +0800

    修正编码格式

--- my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyStudentScoresServiceImpl.java
+++ my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyStudentScoresServiceImpl.java
@@ -1,6 +1,5 @@
 package com.shotgun.my.service.serviceImpl.defaultGroup;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.shotgun.my.api.api.defaultGroup.MyStudentScoresServiceApi;
 import com.shotgun.my.api.po.pojos.defaultGroup.MyStudentScores;
 import com.shotgun.my.service.dao.defaultGroup.MyStudentScoresMapper;
@@ -13,9 +12,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(MyStudentScoresServiceApi.PATH)
 public class MyStudentScoresServiceImpl extends MyServiceImpl<MyStudentScoresMapper, MyStudentScores> implements MyStudentScoresService {
 
-
-    public void a() {
-        IPage<MyStudentScores> myStudentScoresIPage = testGet10();
-    }
 
 }

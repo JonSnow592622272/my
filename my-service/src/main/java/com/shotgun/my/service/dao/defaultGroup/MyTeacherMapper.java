ebec5a66a9843e3ba84a3c58052abc7a1e876cf5
wulingming <wulingming@zhixueyun.com>
2019-12-17 16:28:48 +0800

    优化

---
+++ my-service/src/main/java/com/shotgun/my/service/dao/defaultGroup/MyTeacherMapper.java
@@ -0,0 +1,12 @@
+package com.shotgun.my.service.dao.defaultGroup;
+
+import com.shotgun.my.api.po.pojos.defaultGroup.MyTeacher;
+import com.shotgun.mycommon.service.base.MyBaseMapper;
+
+/**
+* 教师表-Mapper 接口
+*
+*/
+public interface MyTeacherMapper extends MyBaseMapper<MyTeacher> {
+
+}
---
+++ my-service/src/main/java/com/shotgun/my/service/serviceImpl/defaultGroup/MyTeacherServiceImpl.java
@@ -0,0 +1,16 @@
+package com.shotgun.my.service.serviceImpl.defaultGroup;
+
+import com.shotgun.my.api.api.defaultGroup.MyTeacherServiceApi;
+import com.shotgun.my.api.po.pojos.defaultGroup.MyTeacher;
+import com.shotgun.my.service.dao.defaultGroup.MyTeacherMapper;
+import com.shotgun.my.service.service.defaultGroup.MyTeacherService;
+import com.shotgun.mycommon.service.base.MyServiceImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(MyTeacherServiceApi.PATH)
+public class MyTeacherServiceImpl extends MyServiceImpl<MyTeacherMapper, MyTeacher> implements MyTeacherService {
+
+
+}

e017dcf4a630f4206a92dca5c8472c0b68a3c1b9
wulingming <wulingming@zhixueyun.com>
2019-12-20 17:54:45 +0800

    优化

--- my-web/src/main/java/com/shotgun/my/web/controller/HelloController.java
+++ my-web/src/main/java/com/shotgun/my/web/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.shotgun.my.web.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.shotgun.my.api.api.defaultGroup.subGroup.MyTeacherServiceApi;
 import com.shotgun.my.api.api.defaultGroup.subGroup.TestBbbServiceApi;
+import com.shotgun.my.api.po.pojos.defaultGroup.subGroup.MyTeacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @Autowired
-    TestBbbServiceApi testBbbService;
+    private TestBbbServiceApi testBbbService;
+
+    @Autowired
+    private MyTeacherServiceApi myTeacherServiceApi;
 
 
     @RequestMapping("/")
     public String index() {
         System.out.println(testBbbService.bb());
 
+
+        IPage<MyTeacher> myTeacherIPage = myTeacherServiceApi.testGet10();
         return "Greetings from Spring Boot! 祝你好运！";
+    }
+
+    @RequestMapping("/testGet10")
+    public IPage<MyTeacher> testGet10() {
+
+        IPage<MyTeacher> myTeacherIPage = myTeacherServiceApi.testGet10();
+        return myTeacherIPage;
     }
 
 }

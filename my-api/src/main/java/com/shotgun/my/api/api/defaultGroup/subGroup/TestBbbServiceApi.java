64db87049f773aee2858ad6292fc00f92b1f6b7c
wulingming <wulingming@zhixueyun.com>
2019-12-20 16:15:14 +0800

    优化

--- my-api/src/main/java/com/shotgun/my/api/api/defaultGroup/subGroup/TestBbbServiceApi.java
+++ my-api/src/main/java/com/shotgun/my/api/api/defaultGroup/subGroup/TestBbbServiceApi.java
@@ -2,10 +2,8 @@ package com.shotgun.my.api.api.defaultGroup.subGroup;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "TestBbbApi", url = "${feign.baidu.url}")
-@RequestMapping(value = "/test222")
 public interface TestBbbServiceApi {
 
     @PostMapping("/bb")

264b482e569fa738d645c658d98d45c8636e800e
jonsnow <592622272@qq.com>
2020-04-26 22:02:37 +0800

    完善

--- my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/subGroup/MyTeacherService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/subGroup/MyTeacherService.java
@@ -1,13 +1,11 @@
 package com.shotgun.my.service.service.defaultGroup.subGroup;
 
 import com.shotgun.my.api.api.defaultGroup.subGroup.MyTeacherServiceApi;
-import com.shotgun.my.api.po.pojos.defaultGroup.subGroup.MyTeacher;
-import com.shotgun.mycommon.service.base.MyIService;
 
 /**
- * @desc 只提供给service层的接口才放到这里(controller无法访问到)，提供给controller层的接口放在ServiceApi接口
+ * 只提供给service层的接口才放到这里(controller无法访问到)，提供给controller层的接口放在ServiceApi接口
  **/
-public interface MyTeacherService extends MyTeacherServiceApi, MyIService<MyTeacher>  {
+public interface MyTeacherService extends MyTeacherServiceApi/*, MyIService<MyTeacher>只允许当前实现类调用里面的方法，避免其他service实现类调用里面的方法来绕过逻辑验证，*/  {
 
 
 }

7b9ddc6411ab290bbff72d151d7c21e0d42ccb42
jonsnow <592622272@qq.com>
2020-04-26 21:32:34 +0800

    完善说明

--- my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/subGroup/MyTeacherService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/subGroup/MyTeacherService.java
@@ -5,7 +5,7 @@ import com.shotgun.my.api.po.pojos.defaultGroup.subGroup.MyTeacher;
 import com.shotgun.mycommon.service.base.MyIService;
 
 /**
- * @desc 只提供给service层的接口才放到这里，提供给controller层的接口放在ServiceApi接口
+ * @desc 只提供给service层的接口才放到这里(controller无法访问到)，提供给controller层的接口放在ServiceApi接口
  **/
 public interface MyTeacherService extends MyTeacherServiceApi, MyIService<MyTeacher>  {
 

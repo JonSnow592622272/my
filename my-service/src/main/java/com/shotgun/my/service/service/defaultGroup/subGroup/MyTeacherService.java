eab4bacceb5a0cdb4d700fce748e6df31b0f6aef
wulingming <wulingming@zhixueyun.com>
2020-05-21 19:48:47 +0800

    完善

--- my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/subGroup/MyTeacherService.java
+++ my-service/src/main/java/com/shotgun/my/service/service/defaultGroup/subGroup/MyTeacherService.java
@@ -6,6 +6,10 @@ import com.shotgun.my.api.api.defaultGroup.subGroup.MyTeacherServiceApi;
  * @author wulm
  * 只提供给service层的接口才放到这里(controller无法访问到)，提供给controller层的接口放在ServiceApi接口
  * 避免重复定义相同功能接口，比如xxxServiceApi定义批量新增方法1，Service也定义批量新增方法2。此时Service拥有两个批量新增方法，可以去除Service中的接口，仅保留Api中的接口。
+ *
+ * 方法名定义规范：
+ *  #定义监听器消息消费接口：以listen开头
+ *
  **/
 public interface MyTeacherService extends MyTeacherServiceApi {
 

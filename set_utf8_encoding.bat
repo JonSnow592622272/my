9b891f354e9c58cb0c8c788d80b74e0a0f865ecb
Jon Snow <592622272@qq.com>
2025-12-30 19:13:58 +0800

    Create set_utf8_encoding.bat

---
+++ set_utf8_encoding.bat
@@ -0,0 +1,6 @@
+@echo off
+rem Set JAVA_TOOL_OPTIONS environment variable to UTF-8 encoding
+echo Setting JAVA_TOOL_OPTIONS environment variable to UTF-8 encoding...
+setx JAVA_TOOL_OPTIONS "-Dfile.encoding=UTF-8"
+echo Setup completed!
+pause
